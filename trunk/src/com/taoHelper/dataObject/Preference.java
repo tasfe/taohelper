30e534dbc57dcd5067f86add244cf6ed599355ff
peggygao1118@gmail.com <peggygao1118@gmail.com@49fae51d-cc57-6f7e-8c4f-ab8c58c774cf>
2011-11-22 14:55:17 +0000

    spell error

---
+++ trunk/src/com/taoHelper/dataObject/Preference.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.taoHelper.dataObject;
+
+import java.util.HashMap;
+
+/**
+ * @author PeggyGao 用户的购物喜好，用户关注的商品类别
+ */
+public class Preference extends BaseDO {
+
+	private String userNick;
+
+	private HashMap<Number, Integer> preferenceMap = new HashMap<Number, Integer>();
+
+	public String getUserNick() {
+		return userNick;
+	}
+
+	public void setUserNick(String userNick) {
+		this.userNick = userNick;
+	}
+
+	public HashMap<Number, Integer> getReferenceMap() {
+		return referenceMap;
+	}
+
+	public void setReferenceMap(HashMap<Number, Integer> referenceMap) {
+		this.referenceMap = referenceMap;
+	}
+
+}
