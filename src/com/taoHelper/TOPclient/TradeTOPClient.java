4acb47d5dd49e85f11d6155b51da08410866860f
peggygao1118@gmail.com <peggygao1118@gmail.com@49fae51d-cc57-6f7e-8c4f-ab8c58c774cf>
2011-11-26 10:26:08 +0000

    bug fixed

--- src/com/taoHelper/TOPclient/TradeTOPClient.java
+++ src/com/taoHelper/TOPclient/TradeTOPClient.java
@@ -34,7 +34,7 @@ public class TradeTOPClient extends BaseTOPClient {
 		TaobaoClient client = new DefaultTaobaoClient(this.sandboxURl, appKey,
 				appSecret);
 		TradesBoughtGetRequest req = new TradesBoughtGetRequest();
-		req.setFields("seller_nick,buyer_nick,title,type,created,sid,tid,seller_rate,buyer_rate,status,payment,discount_fee,adjust_fee,post_fee,total_fee,pay_time,end_time,modified,consign_time,buyer_obtain_point_fee,point_fee,real_point_fee,received_payment,commission_fee,pic_path,num_iid,num_iid,num,price,cod_fee,cod_status,shipping_type,receiver_name,receiver_state,receiver_city,receiver_district,receiver_address,receiver_zip,receiver_mobile,receiver_phone,orders.title,orders.pic_path,orders.price,orders.num,orders.iid,orders.num_iid,orders.sku_id,orders.refund_status,orders.status,orders.oid,orders.total_fee,orders.payment,orders.discount_fee,orders.adjust_fee,orders.sku_properties_name,orders.item_meal_name,orders.buyer_rate,orders.seller_rate,orders.outer_iid,orders.outer_sku_id,orders.refund_id,orders.seller_type");
+		req.setFields("seller_nick,buyer_nick,title,tid,payment,post_fee,total_fee,end_time,pic_path,num_iid,num");
 
 		try {
 			for (int i = 0; i < status_current.length; i++) {
@@ -60,7 +60,7 @@ public class TradeTOPClient extends BaseTOPClient {
 		TaobaoClient client = new DefaultTaobaoClient(this.sandboxURl, appKey,
 				appSecret);
 		TradesBoughtGetRequest req = new TradesBoughtGetRequest();
-		req.setFields("seller_nick,buyer_nick,title,type,created,sid,tid,seller_rate,buyer_rate,status,payment,discount_fee,adjust_fee,post_fee,total_fee,pay_time,end_time,modified,consign_time,buyer_obtain_point_fee,point_fee,real_point_fee,received_payment,commission_fee,pic_path,num_iid,num_iid,num,price,cod_fee,cod_status,shipping_type,receiver_name,receiver_state,receiver_city,receiver_district,receiver_address,receiver_zip,receiver_mobile,receiver_phone,orders.title,orders.pic_path,orders.price,orders.num,orders.iid,orders.num_iid,orders.sku_id,orders.refund_status,orders.status,orders.oid,orders.total_fee,orders.payment,orders.discount_fee,orders.adjust_fee,orders.sku_properties_name,orders.item_meal_name,orders.buyer_rate,orders.seller_rate,orders.outer_iid,orders.outer_sku_id,orders.refund_id,orders.seller_type");
+		req.setFields("seller_nick,buyer_nick,title,tid,payment,post_fee,total_fee,end_time,pic_path,num_iid,num");
 
 		try {
 			for (int i = 0; i < status_past.length; i++) {
