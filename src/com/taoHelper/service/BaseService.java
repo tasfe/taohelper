e0cb52e5296e9d63927783ea156d6fdea109702e
peggygao1118@gmail.com <peggygao1118@gmail.com@49fae51d-cc57-6f7e-8c4f-ab8c58c774cf>
2011-12-03 06:31:22 +0000

    add product Recommend

--- src/com/taoHelper/service/BaseService.java
+++ src/com/taoHelper/service/BaseService.java
@@ -14,26 +14,25 @@ import com.taoHelper.TOPclient.ShopTOPClient;
 import com.taoHelper.TOPclient.TradeTOPClient;
 import com.taoHelper.dao.BudgetDAO;
 import com.taoHelper.dao.FavoriteItemDAO;
-
+import com.taoHelper.dao.PreferenceDAO;
 
 /**
  * @author PeggyGao
- *
+ * 
  */
 public abstract class BaseService {
 	protected static Logger logger = Logger.getLogger(BaseService.class);
-	
+
 	protected static TradeTOPClient tradeTopClient = new TradeTOPClient();
 	protected static ItemTOPClient itemTopClient = new ItemTOPClient();
-	protected static ItemCatTOPClient itemCatTopClient = new ItemCatTOPClient(); 
+	protected static ItemCatTOPClient itemCatTopClient = new ItemCatTOPClient();
 	protected static FavoriteTOPClient favoriteTopClient = new FavoriteTOPClient();
 	protected static ShopTOPClient shopTopClient = new ShopTOPClient();
 	protected static LogisticsTOPClient logisticsTOPClient = new LogisticsTOPClient();
 	protected static AuthorizeTOPClient authoTOPClient = new AuthorizeTOPClient();
-	
+
 	protected static FavoriteItemDAO favoriteItemDAO = new FavoriteItemDAO();
 	protected static BudgetDAO budgetDAO = new BudgetDAO();
-	
-	
-	
+	protected static PreferenceDAO preDAO = new PreferenceDAO();
+
 }
