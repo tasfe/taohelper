495d6d603edec34d950dbda63f07e5ba6257b34e
peggygao1118@gmail.com <peggygao1118@gmail.com@49fae51d-cc57-6f7e-8c4f-ab8c58c774cf>
2011-12-03 06:27:15 +0000

    add taobao.items.get API for product recommend

--- src/com/taoHelper/TOPclient/ItemTOPClient.java
+++ src/com/taoHelper/TOPclient/ItemTOPClient.java
@@ -3,18 +3,24 @@
  */
 package com.taoHelper.TOPclient;
 
+import java.util.List;
+
 import com.taobao.api.ApiException;
 import com.taobao.api.DefaultTaobaoClient;
 import com.taobao.api.TaobaoClient;
 import com.taobao.api.domain.Item;
 import com.taobao.api.request.ItemGetRequest;
+import com.taobao.api.request.ItemsGetRequest;
 import com.taobao.api.response.ItemGetResponse;
+import com.taobao.api.response.ItemsGetResponse;
 
 /**
  * @author PeggyGao
  * 
  */
 public class ItemTOPClient extends BaseTOPClient {
+
+	private static long SCORE = 15l;
 
 	// 锟斤拷锟絠d锟斤拷询锟斤拷品锟斤拷锟斤拷
 	public Item getItemByIdNumId(Number numId) {
@@ -32,6 +38,39 @@ public class ItemTOPClient extends BaseTOPClient {
 		}
 
 		return null;
+
+	}
+
+	
+	/*
+	 * 根据类别id 查询该类别信用等级大于15的商品，如果没有就将信用等级-1，知道搜索到
+	 */
+	public List<Item> getItemsByCid(long cid) {
+
+		List<Item> itemList = null;
+		TaobaoClient client = new DefaultTaobaoClient(this.inUseURL,
+				this.appKey, this.appSecret);
+		ItemsGetRequest req = new ItemsGetRequest();
+		req.setFields("num_iid,title,nick,pic_url,cid,detail_url,price,delist_time,post_fee,score,volume");
+		req.setCid(cid);
+		req.setStartScore(SCORE);
+
+		try {
+			for (;;) {
+				ItemsGetResponse response = client.execute(req);
+				if (response.getTotalResults() != 0) {
+					itemList = response.getItems();
+					break;
+				} else {
+					req.setStartScore(SCORE - 1);
+				}
+			}
+
+		} catch (ApiException e) {
+			logger.error("taobao.items.get API调用失败", e);
+		}
+
+		return itemList;
 
 	}
 
