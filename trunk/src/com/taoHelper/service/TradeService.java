b1016146c65ffec8398d83f689e0d11f0c392768
peggygao1118@gmail.com <peggygao1118@gmail.com@49fae51d-cc57-6f7e-8c4f-ab8c58c774cf>
2011-12-21 08:44:30 +0000

    bug fixed

--- trunk/src/com/taoHelper/service/TradeService.java
+++ trunk/src/com/taoHelper/service/TradeService.java
@@ -15,135 +15,141 @@ import com.taobao.api.domain.ItemCat;
 import com.taobao.api.domain.Trade;
 
 /**
- * @author PeggyGao
- * ����client��DAO��������ϴ���
- *
+ * @author PeggyGao ����client��DAO��������ϴ���
+ * 
  */
 public class TradeService extends BaseService {
-	
-		
-	/*��õ�ǰ�����ж����������
+
+	/*
+	 * ��õ�ǰ�����ж����������
 	 * ͨ�����TradeTOPClient��getCurrentTrade��õ�ǰ�����ж�������Ϣ
-	 * 
 	 */
-	public List<Trade> getCurrentTrades(String sessionKey){
-		
-		if(null==sessionKey){
-			logger.error("invalid sessionKey "+ sessionKey);
+	public List<Trade> getCurrentTrades(String sessionKey) {
+
+		if (null == sessionKey) {
+			logger.error("invalid sessionKey " + sessionKey);
 			return null;
 		}
-		
+
 		List<Trade> tradeList = new ArrayList<Trade>();
 		tradeList = tradeTopClient.getCurrentTrades(sessionKey);
 		Trade trade;
 		Item item;
-		for(int i = 0; i < tradeList.size();i++){
+		for (int i = 0; i < tradeList.size(); i++) {
 			trade = tradeList.get(i);
-			if(trade==null||trade.getNumIid()==null) continue;		
+			if (trade == null || trade.getNumIid() == null)
+				continue;
 			item = itemTopClient.getItemByIdNumId(trade.getNumIid());
-			//add to preference
+			// add to preference
 			String buyerNick = trade.getBuyerNick();
 			preDAO.createPreference(buyerNick, String.valueOf(item.getCid()));
 		}
-		
+
 		return tradeList;
-		
+
 	}
-	
-	
+
 	/*
 	 * ͨ�����LogisticsTOPClient��getLogisticsInfoByTid���ÿ�ʶ�����������Ϣ
 	 */
-	public LogisticsInfo getCurrentLogisticsInfo(Number tid,String sellerNick){
-		
-		if(null==tid || null == sellerNick){
-			logger.error("invalid parameter " +tid+"  " +sellerNick);
+	public LogisticsInfo getCurrentLogisticsInfo(Number tid, String sellerNick) {
+
+		if (null == tid || null == sellerNick) {
+			logger.error("invalid parameter " + tid + "  " + sellerNick);
 			return null;
 		}
-		
+
 		LogisticsInfo logisticsInfo;
-		logisticsInfo= logisticsTOPClient.getLogisticsInfoByTid(tid, sellerNick);
-		
+		logisticsInfo = logisticsTOPClient.getLogisticsInfoByTid(tid,
+				sellerNick);
+
 		return logisticsInfo;
-		
+
 	}
-	 
-	/*��������ͳ��
-	 * ͨ�����TradeTOPClient��getPastTrades����ͳ����ѽ��
+
+	/*
+	 * ��������ͳ�� ͨ�����TradeTOPClient��getPastTrades����ͳ����ѽ��
 	 */
-	public Map<Integer,Double> getPaymentInMonth(String sessionKey){
-		
-		if(null==sessionKey){
-			logger.error("invalid sessionKey "+ sessionKey);
+	public Map<Integer, Double> getPaymentInMonth(String sessionKey) {
+
+		if (null == sessionKey) {
+			logger.error("invalid sessionKey " + sessionKey);
 			return null;
 		}
-		
+
 		List<Trade> tradeList = new ArrayList<Trade>();
 		tradeList = tradeTopClient.getPastTrades(sessionKey);
-		HashMap<Integer,Double> moneyInMonth = new HashMap<Integer,Double>(); 
-		
+		HashMap<Integer, Double> moneyInMonth = new HashMap<Integer, Double>();
+
 		Trade trade;
 		Integer month;
 		Double money = 0.0;
-		for(int i = 0 ; i <tradeList.size(); i++){
+		for (int i = 0; i < tradeList.size(); i++) {
 			trade = tradeList.get(i);
-			month = trade.getEndTime().getMonth()+1;
-			if(moneyInMonth.get(month)==null){
+			month = trade.getEndTime().getMonth() + 1;
+			if (moneyInMonth.get(month) == null) {
 				moneyInMonth.put(month, Double.valueOf(trade.getPayment()));
-			}else{
-				moneyInMonth.put(month, moneyInMonth.get(month)+Double.valueOf(trade.getPayment()));
+			} else {
+				moneyInMonth.put(
+						month,
+						moneyInMonth.get(month)
+								+ Double.valueOf(trade.getPayment()));
 			}
-		
-			
+
 		}
-		
+
 		return moneyInMonth;
-		
+
 	}
-	
-	/*�����Ŀ���ͳ��
-	 * ͨ�����TradeTOPClient��getPastTrades�����ʷ����
+
+	/*
+	 * �����Ŀ���ͳ�� ͨ�����TradeTOPClient��getPastTrades�����ʷ����
 	 * ͨ��ItemTOPClient�е�getItemByIdNumId���������Ʒ����Ŀid
-	 * ͨ��ItemCatTOPClient�е�getItemCatByCid�����Ʒ��Ŀ
-	 * ���ﶼʹ�ø���Ŀ
+	 * ͨ��ItemCatTOPClient�е�getItemCatByCid�����Ʒ��Ŀ ���ﶼʹ�ø���Ŀ
 	 */
-	public Map<String,Double> getPaymentInCat(String sessionKey){
-		
-		if(null==sessionKey){
-			logger.error("invalid sessionKey "+ sessionKey);
+	public Map<String, Double> getPaymentInCat(String sessionKey) {
+
+		if (null == sessionKey) {
+			logger.error("invalid sessionKey " + sessionKey);
 			return null;
 		}
-		
+
 		List<Trade> tradeList = new ArrayList<Trade>();
 		tradeList = tradeTopClient.getPastTrades(sessionKey);
-		HashMap<String,Double> moneyInCat = new HashMap<String,Double>(); 
-		
+		HashMap<String, Double> moneyInCat = new HashMap<String, Double>();
+
 		Trade trade;
 		Item item;
 		ItemCat itemCat;
 		String cateName;
 		Double money = 0.0;
-		for(int i = 0; i < tradeList.size();i++){
+		for (int i = 0; i < tradeList.size(); i++) {
 			trade = tradeList.get(i);
-			if(trade==null||trade.getNumIid()==null) continue;
+			if (trade == null || trade.getNumIid() == null)
+				continue;
 			item = itemTopClient.getItemByIdNumId(trade.getNumIid());
+			if (null == item || item.getCid() == null)
+				continue;
 			itemCat = itemCatTopClient.getItemCatByCid(item.getCid());
-			
-			//add to preference
+
+			// add to preference
 			String buyerNick = trade.getBuyerNick();
 			preDAO.createPreference(buyerNick, String.valueOf(item.getCid()));
-			
+
 			cateName = itemCat.getName();
-			if(moneyInCat.get(cateName)==null){
+			if (moneyInCat.get(cateName) == null) {
 				moneyInCat.put(cateName, Double.valueOf(trade.getPayment()));
-			}else{
-				moneyInCat.put(cateName, moneyInCat.get(cateName)+Double.valueOf(trade.getPayment()));
+			} else {
+				moneyInCat.put(
+						cateName,
+						moneyInCat.get(cateName)
+								+ Double.valueOf(trade.getPayment()));
 			}
-		
+
 		}
-		
+
 		return moneyInCat;
-		
+
 	}
 
 }
